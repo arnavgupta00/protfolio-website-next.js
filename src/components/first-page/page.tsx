a733a02352c16fab58cbece9c446272f1debac8a
arnavgupta00 <arnavguptagg@gmail.com>
2024-02-06 15:21:47 +0530

    responsive

--- src/components/first-page/page.tsx
+++ src/components/first-page/page.tsx
@@ -1,9 +1,49 @@
 import "@/components/first-page/page.css";
 
 export default function FirstPage() {
+
+  const isMobileOrTablet = window.matchMedia("(max-width: 768px)").matches;
   return (
     <div>
-      <div className="firstPage">
+      {isMobileOrTablet ? (
+        <div className="firstPage" style={{flexDirection:"row-reverse", height:"80vh"}}>
+        
+
+        <div className="fpLeftBlock" style={{width:"40vw" , height:"80vh"}}>
+          <div className="fpageMainTextBlock" style={{flexDirection:"row" , justifyContent:"center", alignItems:"center",width:"100%"}}>
+            <div className="fpageMainTextBlockName" style={{flexDirection:"column" , justifyContent:"flex-start", alignItems:"flex-end",width:"100%"}} >
+              <h2 style={{fontSize:"1.4rem", alignSelf:"flex-end"}}>Arnav Gupta</h2>
+              <h3 style={{fontSize:"0.7rem",alignSelf:"flex-end"}}>Full Stack Developer</h3>
+            </div>
+
+            {/* <div className="resumeLink">
+              <button
+                className="resumeLinkButton"
+                onClick={() =>
+                  window.open(
+                    "https://drive.google.com/file/d/1-abyMw05t0s1E4za9hM9CfHU4_trz8iy/view?usp=sharing",
+                    "_blank"
+                  )
+                }
+              >
+                Resume
+              </button>
+            </div> */}
+          </div>
+        </div>
+        <div className="fpageMainTextBlockTagline" style={{width:"50vw",zIndex:"-1" , alignSelf:"start", position:"relative", left:"4%"}}>
+          <div className="fpageMainTextBlockh1">
+            {"Building Digital Dreams that, Spark Creativity.".split(" ").map((word, index) => (
+              <h1 style={{fontSize:"5rem"}} key={index}>{word} </h1>
+            ))}
+          </div>
+
+        
+        </div>
+      
+      </div>
+      ) : (
+        <div className="firstPage">
         
 
         <div className="fpLeftBlock">
@@ -41,6 +81,8 @@ export default function FirstPage() {
           <div className="imageBlock"></div>
         </div>
       </div>
+      )}
+      
     </div>
   );
 }
--- src/components/navbar/page.tsx
+++ src/components/navbar/page.tsx
@@ -1,35 +1,91 @@
-"use client"
+"use client";
 
-import "@/components/navbar/page.css"
+import "@/components/navbar/page.css";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import React from "react";
 import { aboutrefG, projectrefG } from "../refManagements";
 
-
 export default function Navbar() {
-    const isMobileOrTablet = useMediaQuery("(max-width: 767px)");
-    
-    const scrollToProjects = () => {
-        projectrefG.current?.scrollIntoView({ behavior: "smooth" });
-    };
+  const isMobileOrTablet = useMediaQuery("(max-width: 767px)");
 
-    const scrollToAbout = () => {
-        aboutrefG.current?.scrollIntoView({ behavior: "smooth" });
-    };
+  const scrollToProjects = () => {
+    projectrefG.current?.scrollIntoView({ behavior: "smooth" });
+  };
 
-    return (
-        <div>
-            <div className="navbar">
-                <div className="navbarLeftBox">
-                    <h1>Arnav Gupta</h1>
-                </div>
-                <div className="navbarRightBox">
-                    <button className="button" onClick={scrollToProjects}>Projects</button>
-                    <button className="button" onClick={scrollToAbout}>About</button>
-                    <button className="button" onClick={()=>window.open("https://www.instagram.com/arnav_gupta3035/?hl=en", "_blank")}>Contact</button>
-                    <button className="button" onClick={()=>window.open("https://github.com/arnavgupta00", "_blank")}>GitHub</button>
-                </div>
-            </div>
+  const scrollToAbout = () => {
+    aboutrefG.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  return (
+    <div>
+      {isMobileOrTablet ? (
+        <div className="navbar" style={{width:"105vw"}}>
+        <div className="navbarLeftBox" style={{visibility:"hidden"}}>
+          <h1>Arnav Gupta</h1>
         </div>
-    );
+        <div className="navbarRightBox">
+          <button className="button" onClick={scrollToProjects}>
+            Projects
+          </button>
+          <button className="button" onClick={scrollToAbout}>
+            About
+          </button>
+          <button
+            className="button"
+            onClick={() =>
+              window.open(
+                "https://www.instagram.com/arnav_gupta3035/?hl=en",
+                "_blank"
+              )
+            }
+          >
+            Contact
+          </button>
+          <button
+            className="button"
+            onClick={() =>
+              window.open("https://github.com/arnavgupta00", "_blank")
+            }
+          >
+            GitHub
+          </button>
+        </div>
+      </div>
+      ) : (
+        <div className="navbar">
+          <div className="navbarLeftBox">
+            <h1>Arnav Gupta</h1>
+          </div>
+          <div className="navbarRightBox">
+            <button className="button" onClick={scrollToProjects}>
+              Projects
+            </button>
+            <button className="button" onClick={scrollToAbout}>
+              About
+            </button>
+            <button
+              className="button"
+              onClick={() =>
+                window.open(
+                  "https://www.instagram.com/arnav_gupta3035/?hl=en",
+                  "_blank"
+                )
+              }
+            >
+              Contact
+            </button>
+            <button
+              className="button"
+              onClick={() =>
+                window.open("https://github.com/arnavgupta00", "_blank")
+              }
+            >
+              GitHub
+            </button>
+          </div>
+        </div>
+      )}
+      
+    </div>
+  );
 }
