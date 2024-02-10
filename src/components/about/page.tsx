c2d7e9a684e7ca77959f7e2867deffdbcf259e92
arnavgupta00 <arnavguptagg@gmail.com>
2024-02-10 22:19:14 +0530

    responsive upgrade 3 final

--- src/components/about/page.tsx
+++ src/components/about/page.tsx
@@ -5,14 +5,10 @@ import { useState, useRef, useEffect } from "react";
 import { aboutRefAdd } from "@/components/refManagements";
 import useMediaQuery from "@mui/material/useMediaQuery";
 
-
-
-
 export default function About() {
   const [scrollPosition, setScrollPosition] = useState(0);
   const aboutRef = useRef<HTMLDivElement>(null);
   const isMobileOrTablet = useMediaQuery("(max-width: 767px)");
-
 
   useEffect(() => {
     aboutRefAdd(aboutRef);
@@ -40,8 +36,13 @@ export default function About() {
           style={{
             position: "absolute",
             zIndex: "-1",
-            marginLeft: isMobileOrTablet ? (scrollPosition > 35 ? `${-200 + scrollPosition * 3}%` : "-200%") : (scrollPosition > 35 ? `${-200 + scrollPosition * 2}%` : "-200%"),
-              
+            marginLeft: isMobileOrTablet
+              ? scrollPosition > 35
+                ? `${-200 + scrollPosition * 2.5}%`
+                : "-200%"
+              : scrollPosition > 35
+              ? `${-200 + scrollPosition * 2}%`
+              : "-200%",
           }}
         >
           <h1>About me</h1>
@@ -54,19 +55,33 @@ export default function About() {
               <div className="timeline-middle">
                 <div className="timeline-circle"></div>
               </div>
-              <div className="timeline-component timeline-content">
+              <div
+                className="timeline-component timeline-content"
+                style={{ padding: "30px" }}
+              >
                 <h3>Education</h3>
-                <p>
-                  Some TextSome TextSome TextSome TextSome TextSome TextSome
-                  TextSome Text
+                <p style={{ fontFamily: "verdana", marginTop: "10px" }}>
+                  Currently pursuing B.Tech in Computer Science and Engineering
+                  at Thapar Institute of Engineering and Technology, Patiala.
                 </p>
               </div>
-              <div className="timeline-component timeline-content">
+              <div
+                className="timeline-component timeline-content"
+                style={{ padding: "30px", paddingLeft: "40px" }}
+              >
                 <h3>Tech Stack</h3>
-                <p>
-                  Some TextSome TextSome TextSome TextSome TextSome TextSome
-                  TextSome TextSome TextSome TextSome TextSome TextSome TextSome
-                  TextSome TextSome TextSome TextSome Text
+                <p style={{ fontFamily: "verdana", marginTop: "10px" }}>
+                  <ul>
+                    <li>
+                      Frontend: React, Next.js, HTML, CSS, JavaScript,
+                      TypeScript
+                    </li>
+                    <li>
+                      Backend: WebSocket's , WebRTC , Node.js, Express.js,
+                      MongoDB
+                    </li>
+                    <li>Others: Git, Docker , Selenium Web driver , Kivy </li>
+                  </ul>
                 </p>
               </div>
               <div className="timeline-middle">
@@ -79,12 +94,15 @@ export default function About() {
               <div className="timeline-middle">
                 <div className="timeline-circle"></div>
               </div>
-              <div className="timeline-component timeline-content">
+              <div
+                className="timeline-component timeline-content"
+                style={{ padding: "30px", paddingLeft: "40px" }}
+              >
                 <h3>Personality </h3>
-                <p>
-                  Some TextSome TextSome TextSome TextSome TextSome TextSome
-                  TextSome TextSome TextSome TextSome TextSome TextSome TextSome
-                  TextSome TextSome TextSome TextSome Text
+                <p  style={{ fontFamily: "verdana", marginTop: "10px" }}>
+                  I possess a calm and composed nature, coupled with dedication
+                  and hard work. My skills are complemented by a quick learning
+                  ability, allowing me to adapt efficiently to new tasks.
                 </p>
               </div>
             </div>
